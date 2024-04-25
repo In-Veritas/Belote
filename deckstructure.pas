7cce9523a7a6d5f9a4c37414918f501697b003cf
Nijiiiiiii <163537101+Nijiiiiiii@users.noreply.github.com>
2024-04-25 14:46:34 +0200

    Update deckstructure.pas

--- deckstructure.pas
+++ deckstructure.pas
@@ -6,8 +6,57 @@ interface
 uses
   Classes, SysUtils;
 
+type
+  typeentier=integer; //Type afin de facilité l'usage de la procedure permuter
+  typetableau= array [1..32] of integer; //Type permettant l'usage des foonctions 
+
 implementation
+
+
 begin
+
+procedure permuter (var X,Y:typeentier); //procedure qui échange deux tèrmes
+VAR
+  Z:typeentier;
+begin
+  Z:=X;
+  X:=Y;
+  Y:=Z;
+end;
+
+
+function creertableau():typetableau; //procédure qui crée un tableau trier
+VAR
+  T:typetableau;
+  I:integer;
+
+begin
+  for I:=1 to 32 do
+    begin
+      T[I]:=I;
+    end;
+
+  creertableau:=T;
+end;
+
+function melangertableau(N:integer):typetableau; //procédure qui mélange un taableau, N sert à selon si la personne mélange beaucoup ou pas
+VAR
+  T:typetableau;
+  I,X,Y:integer;
+
+begin
+  T:=creertableau(); //on commence par initier le tableau
+
+  for I:=1 to N do //dépend du nombre de mélange
+    begin
+      X:=random(31)+1;
+      Y:=random(31)+1;
+
+      permuter(T[X],T[Y]); 
+    end;
+
+  melangertableau:=T;
+end;
 
 end.
 
